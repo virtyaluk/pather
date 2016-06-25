aacd3cce65b7d5ac64d06dd0a15c5560f6360643
Bohdan Shtepan <winsatana@ya.ru>
2016-06-25 03:19:12 +0300

    Fixes indentation

--- cli.js
+++ cli.js
@@ -1,25 +1,25 @@
 const fs = require('fs'),
-	pather = require('./pather'),
+    pather = require('./pather'),
     inputFile = process.argv[2] || null,
-	outputFile = process.argv[3] || 'output.txt',
-	writeFile = function(err, data) {
-		if (err) { return console.error(err); }
-		
-		console.info('All OK. Check the `' + outputFile + '` file.');
-	},
-	readFile = function(err, data) {
-		if (err) { return console.error(err); }
-		
-		fs.writeFile(outputFile, pather(data), writeFile);
-	},
-	fileExists = function(err, data) {
-		if (err) { return console.error(err); }
-		
-		fs.readFile(inputFile, 'utf8', readFile);
-	};
+    outputFile = process.argv[3] || 'output.txt',
+    writeFile = function(err, data) {
+        if (err) { return console.error(err); }
+
+        console.info('All OK. Check the `' + outputFile + '` file.');
+    },
+    readFile = function(err, data) {
+        if (err) { return console.error(err); }
+
+        fs.writeFile(outputFile, pather(data), writeFile);
+    },
+    fileExists = function(err, data) {
+        if (err) { return console.error(err); }
+
+        fs.readFile(inputFile, 'utf8', readFile);
+    };
 
 if (inputFile) {
-	fs.access(inputFile, fs.R_OK, fileExists);
+    fs.access(inputFile, fs.R_OK, fileExists);
 } else {
-	console.log('PROBLEM: no input file is not given');
+    console.log('PROBLEM: no input file is not given');
 }
--- pather.js
+++ pather.js
@@ -1,48 +1,46 @@
 const littleMagic = function(line, start, end) {
-	return line.map(function(li, ii) {
-		if (ii >= start && ii <= end) {
-			if (li !== '#') {
-				return '*';
-			}
-		}
-		
-		return li;
-	});
+    return line.map(function(li, ii) {
+        if (ii >= start && ii <= end && li !== '#') {
+            return '*';
+        }
+
+        return li;
+    });
 };
 
 module.exports = function pather(data) {
-	const lines = data.split('\n');
-	let firstLine = null,
-		lastLine = null,
-		lastHashPos = null;
-		
-	lines.forEach(function(line, i) {
-		const oc = line.indexOf('#');
-		
-		if (oc !== -1) {
-			if (!firstLine) {
-				firstLine = lastLine = i;
-			} else {
-				lastLine = i;
-			}
-		}
-	});
-	
-	return lines.map(function(line, i) {
-		let l = line.split(''),
-			lHash = null;
-		const firstHash = l.indexOf('#');
-		
-		if (i >= firstLine && i <= lastLine) {
-			if (firstHash !== -1) {
-				lHash = l.lastIndexOf('#');
-				l = littleMagic(l, lastHashPos ? Math.min(firstHash, lastHashPos, lHash) : firstHash, Math.max(firstHash, lastHashPos, lHash));
-				lastHashPos = firstHash < lastHashPos ? firstHash : lHash;
-			} else if (lastHashPos !== null && firstHash === -1) {
-				l[lastHashPos] = '*';
-			}
-		}
-		
-		return l.join('');
-	}).join('\n');
+    const lines = data.split('\n');
+    let firstLine = null,
+        lastLine = null,
+        lastHashPos = null;
+
+    lines.forEach(function(line, i) {
+        const oc = line.indexOf('#');
+
+        if (oc !== -1) {
+            if (!firstLine) {
+                firstLine = lastLine = i;
+            } else {
+                lastLine = i;
+            }
+        }
+    });
+
+    return lines.map(function(line, i) {
+        let l = line.split(''),
+            lHash = null;
+        const firstHash = l.indexOf('#');
+
+        if (i >= firstLine && i <= lastLine) {
+            if (firstHash !== -1) {
+                lHash = l.lastIndexOf('#');
+                l = littleMagic(l, lastHashPos ? Math.min(firstHash, lastHashPos, lHash) : firstHash, Math.max(firstHash, lastHashPos, lHash));
+                lastHashPos = firstHash < lastHashPos ? firstHash : lHash;
+            } else if (lastHashPos !== null && firstHash === -1) {
+                l[lastHashPos] = '*';
+            }
+        }
+
+        return l.join('');
+    }).join('\n');
 };
